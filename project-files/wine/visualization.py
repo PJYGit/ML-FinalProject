479312b883e31f336112f2aee8c9f2f17e0120af
unknown <870096160@qq.com>
2019-10-25 19:06:49 -0400

    Init visualization function.

---
+++ project-files/wine/visualization.py
@@ -0,0 +1,24 @@
+import pandas
+from pandas.plotting import scatter_matrix
+import matplotlib.pyplot as plt
+from sklearn import model_selection
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+
+file = "D:\\My Desktop\\WLU\\CP322-Machine Learning\\FP\\dataset\\winequality.data"
+# file = "D:\\My Desktop\\WLU\\CP322-Machine Learning\\FP\\dataset\\iris.data"
+names = ["fixed acidity", "volatile acidity", "citric acid", "residual sugar", "chlorides", "free sulfur dioxide",
+         "total sulfur dioxide", "density", "pH", "sulphates", "alcohol", "quality"]
+# names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+
+dataset = pandas.read_csv(file, names=names)
+pandas.set_option('display.width', None)
+
+print(dataset.head(2))
